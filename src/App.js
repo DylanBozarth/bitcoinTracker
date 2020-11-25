793c0e22d16e68e8bd1c399a63e1ed57bcb3ec5d
dylan <dylanabozarth@gmail.com>
2020-11-25 09:47:30 -0800

    working api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-import React, {useState, useEffect} from 'react'
+const Bitcoin = () => {
+  const [data, setData] = useState({});
 
-import './App.css';
+  useEffect(() => {
+    fetch("https://blockchain.info/ticker") // Call the fetch function passing the url of the API as a parameter
+      .then((resp) => resp.json()) // Transform the data into json
+      .then((resp) => {
+        console.log(resp);
+        setData(resp);
+      });
+  }, []);
 
-
-
-function Bitcoin() {
-  
-  fetch('https://blockchain.info/ticker') // Call the fetch function passing the url of the API as a parameter
-  .then((resp) => resp.json()) // Transform the data into json
-.then(function(resp) {
-    setData({data: resp.json()})
-    console.log(data)
-    
-})
-  
-const [data, setData] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        
-       
-      
-        
-      </header>
+      <header className="App-header"></header>
     </div>
   );
-}
+};
 
 export default Bitcoin;
