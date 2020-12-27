f7b29822e349ea802bf7fe7c122a8d3e1870f020
dylan <dylanabozarth@gmail.com>
2020-12-26 20:10:04 -0800

    photos and data setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from "react";
 
 const Bitcoin = () => {
   const [data, setData] = useState(null);
-
+  const [EthData, setEthData] = useState(null);
+  const [dogeData, setDogeData] = useState(null);
   useEffect(() => {
     fetch("https://blockchain.info/ticker") // Call the fetch function passing the url of the API as a parameter
       .then((resp) => resp.json()) // Transform the data into json
@@ -14,34 +15,37 @@ const Bitcoin = () => {
   }, []);
   if (data !== null) {
     return (
-      <div className="App container-fluid">
-        <div className="row text-center">
-          <div className="col-sm-4 bitcoin">
+      <div className="App container text-center homepagebox">
+        <div className="row ">
+          <div className="col-sm-4 bitcoin ">
             <img
               src="./bitcoin.png"
               width="100px"
               height="100px"
               className="img-fluid"
+              alt="bitcoin"
             ></img>
             1 Bitcoin = ${data.USD.buy}{" "}
           </div>
           <div className="col-sm-4 bitcoin">
             <img
-              src="./bitcoin.png"
+              src="./eth.png"
               width="100px"
               height="100px"
               className="img-fluid"
+              alt="eth"
             ></img>
-            1 Bitcoin = ${data.USD.buy}{" "}
+            1 Etherium = ${EthData}{" "}
           </div>
           <div className="col-sm-4 bitcoin">
             <img
-              src="./bitcoin.png"
+              src="./doge.png"
               width="100px"
               height="100px"
+              alt="doge"
               className="img-fluid"
             ></img>
-            1 Bitcoin = ${data.USD.buy}{" "}
+            1 DogeCoin = ${dogeData}{" "}
           </div>
         </div>
       </div>
